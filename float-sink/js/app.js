f2974eb40de75c052569bc5b91e77acf873fb272
Nan <nancy@nangoo.org>
2014-05-13 14:59:59 -0400

    Fixing the layout...

--- float-sink/js/app.js
+++ float-sink/js/app.js
@@ -1,24 +1,20 @@
 angular.module("reviewApp", ["firebase"])
   .factory("Unbottled", ["$firebase", function($firebase) {
-    var ref = new Firebase("https://unbottledd.firebaseio.com/");
+    var ref = new Firebase("https://unbottledd.firebaseio.com");
     return $firebase(ref);
   }])
   .controller("ReviewsController", ["$scope", "Unbottled",
     function($scope, unbottled) {
-
       // on "change" do stuff.
       unbottled.$on("change", function() {
         $scope.unbottled = unbottled;
-
         // Get the keys to create school options.
         var schools = {};
-
         for (schoolKey in unbottled) {
           // Filter only our school objects.
           if (typeof unbottled[schoolKey] === "object" && schoolKey !== "$id"){
             // Create our options array for our dropdown.
             schools[schoolKey] = unbottled[schoolKey];
-
             // Count and prep our data.
             schools[schoolKey].totalVotes = 0;
             schools[schoolKey].yesVotes = 0;
@@ -35,10 +31,15 @@ angular.module("reviewApp", ["firebase"])
             }
           }
         }
-
         // Add our options to scope.
         $scope.schools = schools;
       });
+      // Casting Votes.
+
+      $scope.castvote = function(poll){
+        $scope.poll = poll;
+        $scope.saveData();
+      }
 
       // Save our data.
       $scope.saveData = function() {
@@ -46,13 +47,11 @@ angular.module("reviewApp", ["firebase"])
         var data = {
           poll: $scope.poll
         };
-
         // Save our data.
         $scope.unbottled.$child($scope.schoolChoice).$child('votes').$add(data);
-
         // Reset our form values.
         // $scope.schoolChoice = '';
-        $scope.poll = '';
+        $scope.poll = null;
       }
     }
   ]);
