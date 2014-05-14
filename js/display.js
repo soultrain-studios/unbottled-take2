aead2c9d2f637f266e39fe700662b0f3e6c5ee86
Nan <nancy@nangoo.org>
2014-05-14 06:58:15 -0400

    Modal finished, show/hide school stats finished

--- js/display.js
+++ js/display.js
@@ -5,9 +5,24 @@ $(function() {
 			scrollTop: anchor.offset().top
 		},1000);
 
-		var schoolStats = $("#stats-school_"+this.value);
+		var schoolStats = $("#stats-"+this.value);
 		schoolStats.slideToggle();
 
 		return false;
 	});
-})
+});
+
+var myDataRef = new Firebase('https://waterfact.firebaseio.com/');
+
+var messages = [];
+myDataRef.on('child_added', function(snapshot) {
+	messages.push(snapshot.val());
+
+	var randActive = Math.floor(Math.random() * messages.length);
+
+	displayMessage(messages[randActive].fact);
+});
+      //only display newest comment
+      function displayMessage(fact) {
+      	$('#message').text(fact);
+      };
